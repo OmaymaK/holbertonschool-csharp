13e40117ff3b1671dc33ea662b12c9b94c3f92c1
OmaymaK <emmablink050@gmail.com>
2022-12-01 15:07:34 +0100

    task 4

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 99; i++)
+            {
+                string hex = Convert.ToString(i, 16);
+                Console.Write($"{i} = 0x{hex}\r\n");
+            }
+        }
+    }
+}
