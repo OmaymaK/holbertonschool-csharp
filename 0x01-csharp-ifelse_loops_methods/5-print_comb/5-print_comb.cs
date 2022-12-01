89e4e086a83b7e662344bf92361eb744a724aa21
OmaymaK <emmablink050@gmail.com>
2022-12-01 17:43:28 +0100

    print comb

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                string ch = i.ToString("00");
+                if (i == 99)
+                {
+                    Console.Write($"{ch}\r\n");
+                }
+                else
+                {
+                    Console.Write($"{ch}, ");
+                }
+            }
+        }
+    }
+}
