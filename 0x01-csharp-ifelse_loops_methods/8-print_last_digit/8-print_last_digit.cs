a9e18de6020cfc3a027708b993c6da8e584f5b8b
OmaymaK <emmablink050@gmail.com>
2022-12-03 14:28:06 +0100

    task 8

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _8_print_last_digit
+{
+    class Number
+    {
+        public static int PrintLastDigit(int number)        
+        {
+            int r = Math.Abs(number) % 10;
+            Console.Write(r);
+        }
+    }
+}
