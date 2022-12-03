4ad5134fe4acc7c7682dc8c723d40ca2a054d093
OmaymaK <emmablink050@gmail.com>
2022-12-03 15:09:33 +0100

    task 11

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _11_print_diagonal
+{
+    class Line
+    {
+        public static void PrintDiagonal(int length)
+        {
+            if (length <= 0)
+            {
+                Console.WriteLine();
+            }
+            for (int i = 1; i < length + 1; i++)
+            {
+                Console.WriteLine("\\");
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
