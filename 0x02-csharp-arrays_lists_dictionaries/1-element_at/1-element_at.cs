4795e3bad8b60ac88384ff3e08a7eece111f46c5
OmaymaK <emmablink050@gmail.com>
2022-12-04 15:36:47 +0100

    task 1

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1_element_at
+{
+    class Array
+    {
+        public static int elementAt(int[] array, int index)
+        {
+            if (index > array.Length)
+            {
+                Console.WriteLine("Index out of range");
+                return -1;
+            }
+            else
+            {
+                return array[index];
+            }
+        }
+    }
+}
