010fa39d0280561cd47e6d35bc58d7691b19e058
OmaymaK <emmablink050@gmail.com>
2022-12-04 16:58:13 +0100

    fix

--- 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -4,6 +4,11 @@ class Array
 {
     public static void Reverse(int[] array)
     {
+        if (array == null)
+        {
+            Console.WriteLine();
+            return;
+        }
         if (array.Length > 0)
         {
             for (int i = array.Length - 1; i == 0; i--)
@@ -16,7 +21,7 @@ class Array
         }
         else
         {
-            Console.WriteLine ();
+            Console.WriteLine();
         }
     }
-
+}
