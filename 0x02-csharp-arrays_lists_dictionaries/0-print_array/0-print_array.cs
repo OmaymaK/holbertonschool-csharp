6949d044fdfe0532fe17afc6d25f6858bb604aba
OmaymaK <emmablink050@gmail.com>
2022-12-04 13:34:49 +0100

    task 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _0_print_array
+{
+    class Array
+    {
+        public static int[] CreatePrint(int size)
+        {
+            if (size == 0)
+                Console.WriteLine();
+            else if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }
+            else 
+            {
+                int[] arr = new int[size];
+                for (int i = 0; i < size - 1; i++)
+                {
+                    arr[i] = i;
+                    Console.Write($"{arr[i]} ");
+                }
+                Console.Write($"{arr[size]}");
+            }
+        }
+    }
+}
