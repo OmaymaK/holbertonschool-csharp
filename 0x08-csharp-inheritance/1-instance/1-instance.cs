074b208f0e9162600d3132a63c2511960ac2033f
OmaymaK <emmablink050@gmail.com>
2022-12-20 20:21:39 +0100

    task1

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+    class Obj
+    {
+        /// <summary>
+        /// check if obj insatnce of array or sub
+        /// </summary>
+        /// <param name="obj">obj to check</param>
+        /// <returns> True or  False </returns>
+        public static bool IsInstanceOfArray(object obj)
+        {
+            if (obj is Array || obj.GetType() == typeof(Array))
+                return true;
+            else
+                return false;
+        }
+    }
