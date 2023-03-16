cc05500464ed410c7ea0de51c3f2449ef25642fd
OmaymaK <emmablink050@gmail.com>
2023-03-16 16:33:03 +0100

    get set

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+/// <summary>
+/// Abstract Class.
+/// </summary>
+
+abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        return ($"{name} is a {this.GetType()}");
+    }
+}
